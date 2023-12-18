6dbe359e3c480f2075d3cf2e3786c14e5a86c178
Ubuntu <ubuntu@ip-172-31-40-37.ap-southeast-1.compute.internal>
2023-12-18 10:10:59 +0000

    1st commit

---
+++ test_atg_world.py
@@ -0,0 +1,9 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+class TestAtgWorld(unittest.TestCase):
+    def setUp(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        self.driver = webdriver.Chrome(options=chrome_options)
